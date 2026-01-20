bb42099c69cab590ee4ad8eeaf30c2636d21a232
nakulj <3213360+nakulj@users.noreply.github.com>
2026-01-20 13:57:48 -0800

    Revert PR #8
    Seems like this has broken prompts and some workflows, reverting for
    now.

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # auto-venv
 Automatically activate virtual environments in fish. Based on gists by [bastibe](https://gist.github.com/bastibe/c0950e463ffdfdfada7adf149ae77c6f) and [tommyip](https://gist.github.com/tommyip/cf9099fa6053e30247e5d0318de2fb9e).
 
-## Features
-
-- Automatically activates Python virtual environments when entering a directory containing a venv
-- Automatically deactivates virtual environments when leaving the directory
-- Safely handles virtual environment inheritance in new shell sessions
-- Prevents shell exits during activation/deactivation
-- Works with common virtual environment directory names: `env`, `.env`, `venv`, `.venv`
-- Supports git repositories by detecting the repository root
-
 ## Installation
 
 ### Using [`fisher`](https://github.com/jorgebucaran/fisher) (Recommended):
@@ -21,21 +12,3 @@ fisher install nakulj/auto-venv
 ### Manual install
 
 Copy `conf.d/venv.fish` into your `conf.d` directory (usually `~/.config/fish/conf.d`)
-
-## How it works
-
-The plugin uses fish shell events and directory monitoring to:
-
-1. Detect when you change directories and automatically activate/deactivate virtual environments
-2. Handle virtual environment inheritance in new shell sessions safely
-3. Implement safe activation/deactivation that won't cause the shell to exit
-
-### Virtual Environment Detection
-
-The plugin looks for virtual environments in the following directories:
-- `env/`
-- `.env/`
-- `venv/`
-- `.venv/`
-
-If you're in a git repository, it will look for these directories at the repository root.
--- conf.d/venv.fish
+++ conf.d/venv.fish
@@ -6,113 +6,32 @@
 # * Instead of overriding cd, we detect directory change. This allows the script to work
 #   for other means of cd, such as z.
 # * Update syntax to work with new versions of fish.
-# * Prevent shell from exiting when deactivating virtualenv.
-# * Only run auto_source_venv if we're not already handling venv.
-
-# Global flag to track if we're in the middle of handling venv
-set -g __VENV_HANDLING 0
-
-function __safe_activate_venv
-    # Save current state
-    set -l old_path $PATH
-    set -l old_pythonhome $PYTHONHOME
-
-    # Read the activate.fish content and extract key parts
-    set -l activate_script (cat $argv[1])
-
-    # Set up the environment without sourcing activate.fish
-    set -gx VIRTUAL_ENV (dirname (dirname $argv[1]))
-    set -gx _OLD_VIRTUAL_PATH $PATH
-    set -gx PATH "$VIRTUAL_ENV/bin" $PATH
-
-    # Handle PYTHONHOME
-    if set -q PYTHONHOME
-        set -gx _OLD_VIRTUAL_PYTHONHOME $PYTHONHOME
-        set -e PYTHONHOME
-    end
-
-    # Set prompt
-    set -gx VIRTUAL_ENV_PROMPT (basename "$VIRTUAL_ENV")
-end
-
-function __handle_virtualenv_inheritance --on-event fish_prompt
-    # Only run in new shells that have inherited VIRTUAL_ENV
-    if test -n "$VIRTUAL_ENV" -a -z "$__VENV_INITIALIZED" -a "$__VENV_HANDLING" -eq 0
-        # Mark this shell as initialized and prevent recursive handling
-        set -g __VENV_INITIALIZED 1
-        set -g __VENV_HANDLING 1
-
-        # Clear inherited environment
-        set -l old_path $PATH
-        set -e VIRTUAL_ENV
-        set -e _OLD_VIRTUAL_PATH
-        set -e _OLD_VIRTUAL_PYTHONHOME
-        set -e PYTHONHOME
-        set -e VIRTUAL_ENV_PROMPT
-        set -gx PATH $old_path
-
-        # Let auto_source_venv handle activation if needed
-        __auto_source_venv
-
-        set -g __VENV_HANDLING 0
-    end
-end
 
 function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
-    # Prevent running during command substitution or if we're already handling venv
-    status --is-command-substitution; and return
-    test "$__VENV_HANDLING" -eq 1; and return
+  status --is-command-substitution; and return
 
-    set -g __VENV_HANDLING 1
+  # Check if we are inside a git repository
+  if git rev-parse --show-toplevel &>/dev/null
+    set dir (realpath (git rev-parse --show-toplevel))
+  else
+    set dir (pwd)
+  end
 
-    # Check if we are inside a git repository
-    if command git rev-parse --show-toplevel &>/dev/null
-        set dir (realpath (command git rev-parse --show-toplevel))
-    else
-        set dir (pwd)
+  # Find a virtual environment in the directory
+  set VENV_DIR_NAMES env .env venv .venv
+  for venv_dir in $dir/$VENV_DIR_NAMES
+    if test -e "$venv_dir/bin/activate.fish"
+      break
     end
+  end
 
-    # Find a virtual environment in the directory
-    set -l VENV_DIR_NAMES env .env venv .venv
-    set -l venv_dir ""
-    for name in $VENV_DIR_NAMES
-        if test -e "$dir/$name/bin/activate.fish"
-            set venv_dir "$dir/$name"
-            break
-        end
-    end
-
-
-    if test -n "$venv_dir" -a "$VIRTUAL_ENV" != "$venv_dir" -a -e "$venv_dir/bin/activate.fish"
-        # Activate venv if it was found and not activated before
-        __safe_activate_venv "$venv_dir/bin/activate.fish"
-    else if test -n "$VIRTUAL_ENV" -a -z "$venv_dir"
-        # Deactivate venv if it is activated but we're no longer in a directory with a venv
-        # Save PATH before deactivation
-        set -l old_path $PATH
-
-        # Try to deactivate safely
-        if functions -q deactivate
-            functions -q deactivate; and deactivate
-            # Restore PATH if it was unset
-            if test -z "$PATH"
-                set -gx PATH $old_path
-            end
-        else
-            # Manual cleanup if deactivate isn't available
-            set -e VIRTUAL_ENV
-            set -e _OLD_VIRTUAL_PATH
-            set -e _OLD_VIRTUAL_PYTHONHOME
-            set -e PYTHONHOME
-            set -e VIRTUAL_ENV_PROMPT
-            set -gx PATH $old_path
-        end
-    end
-
-    set -g __VENV_HANDLING 0
+  # Activate venv if it was found and not activated before
+  if test "$VIRTUAL_ENV" != "$venv_dir" -a -e "$venv_dir/bin/activate.fish"
+    source $venv_dir/bin/activate.fish
+  # Deactivate venv if it is activated but the directory doesn't exist
+  else if not test -z "$VIRTUAL_ENV" -o -e "$venv_dir"
+    deactivate
+  end
 end
 
-# Only run initial check if we're not already handling venv
-if test "$__VENV_HANDLING" -eq 0
-    __auto_source_venv
-end
+__auto_source_venv
