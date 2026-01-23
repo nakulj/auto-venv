93de7352a82f5c43631515bb4bd31d7738a50fae
nakulj <3213360+nakulj@users.noreply.github.com>
2026-01-23 11:05:14 -0800

    refactor and test

--- conf.d/venv.fish
+++ conf.d/venv.fish
@@ -7,28 +7,7 @@
 #   for other means of cd, such as z.
 # * Update syntax to work with new versions of fish.
 
-
-function __venv_base
-  git rev-parse --show-toplevel 2>/dev/null; or pwd
-end
-
-function __handle_venv_activation --argument dir 
-  # Find a virtual environment in the directory
-  set VENV_DIR_NAMES env .env venv .venv
-  for venv_dir in $dir/$VENV_DIR_NAMES
-    if test -e "$venv_dir/bin/activate.fish"
-      break
-    end
-  end
-
-  # Activate venv if it was found and not activated before
-  if test "$VIRTUAL_ENV" != "$venv_dir" -a -e "$venv_dir/bin/activate.fish"
-    source $venv_dir/bin/activate.fish
-  # Deactivate venv if it is activated but the directory doesn't exist
-  else if not test -z "$VIRTUAL_ENV" -o -e "$venv_dir"
-    deactivate
-  end
-end
+source ./venv_helpers.fish
 
 function __auto_source_venv --on-variable PWD --description "Activate/Deactivate virtualenv on directory change"
   status --is-command-substitution; and return
--- tests/venv.test.fish
+++ tests/venv.test.fish
@@ -1,4 +1,6 @@
-source (dirname (status -f))/../conf.d/venv.fish
+source (dirname (status -f))/../conf.d/venv_helpers.fish
+
+# TODO: figure out if these tests can be less stateful
 
 set tmp (mktemp -d)
 
@@ -7,14 +9,38 @@ set tmp (mktemp -d)
 cd $tmp
 @test "returns PWD outside git repo" (__venv_base) -ef $tmp
 
-git init git_repo > /dev/null
+git init $tmp/git_repo > /dev/null
 cd git_repo
 
 @test "returns git root at git root" (__venv_base) -ef "$tmp/git_repo"
 
-mkdir git_repo_dir
-cd git_repo_dir
+mkdir $tmp/git_repo/dir_in_repo
+cd $tmp/git_repo/dir_in_repo
 
 @test "returns git root inside git repo" (__venv_base) -ef "$tmp/git_repo"
 
+# __venv tests
+
+cd $tmp
+
+@test "no output if no virtual env found" (__venv $tmp) -z
+
+mkdir -p venv/bin
+touch venv/bin/activate.fish
+
+
+@test "returns venv directory" (__venv $tmp) -ef "$tmp/venv"
+
+# __handle_venv_activation tests
+
+# Mocks 
+function source
+  echo "sourced $argv"
+end
+
+function deactivate
+  echo "deactivated"
+end
+
+@test "new environment is activated" (__handle_venv_activation $tmp) = "sourced $tmp/venv/bin/activate.fish"
 
