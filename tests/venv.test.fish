5547be84efb15bc042ccd42dc3537776a3621575
nakulj <3213360+nakulj@users.noreply.github.com>
2026-01-25 15:02:17 -0800

    add test

--- tests/venv.test.fish
+++ tests/venv.test.fish
@@ -11,38 +11,46 @@ set tmp (mktemp -d)
   __venv_base
 ) -ef $tmp
 
-git init $tmp/git_repo > /dev/null
-cd git_repo
 
-@test "returns git root at git root" (__venv_base) -ef "$tmp/git_repo"
+@test "returns git root at git root" (
+  git init $tmp/git_repo > /dev/null
+  cd git_repo
+  __venv_base
+) -ef "$tmp/git_repo"
 
-mkdir $tmp/git_repo/dir_in_repo
-cd $tmp/git_repo/dir_in_repo
 
-@test "returns git root inside git repo" (__venv_base) -ef "$tmp/git_repo"
+@test "returns git root inside git repo" (
+  mkdir $tmp/git_repo/dir_in_repo
+  cd $tmp/git_repo/dir_in_repo
+  __venv_base
+) -ef "$tmp/git_repo"
 
 # __venv tests
 
-cd $tmp
-
 @test "no output if no virtual env found" (__venv $tmp) -z
 
-mkdir -p venv/bin
-touch venv/bin/activate.fish
-
-
-@test "returns venv directory" (__venv $tmp) -ef "$tmp/venv"
+@test "returns venv directory" (
+  mkdir -p $tmp/venv/bin
+  touch $tmp/venv/bin/activate.fish
+  __venv $tmp
+) -ef "$tmp/venv"
 
 # __handle_venv_activation tests
-
-# Mocks 
-function source
-  echo "sourced $argv"
-end
 
 function deactivate
   echo "deactivated"
 end
 
-@test "new environment is activated" (__handle_venv_activation $tmp) = "sourced $tmp/venv/bin/activate.fish"
+function source
+  if string match -q '*activate.fish' $argv[1]
+    echo "sourced $argv"
+    return
+  end
+  builtin source $argv
+end
 
+
+@test "new environment is activated" (
+  mkdir -p $tmp/venv/bin
+  __handle_venv_activation $tmp
+) = "sourced $tmp/venv/bin/activate.fish"
